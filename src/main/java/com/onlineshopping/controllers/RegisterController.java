cba0e4873ad5bdae3457a6016f815a02eaa94edf
Bodea Razvan-Marius <razvan.bodea2700@gmail.com>
2021-12-20 21:41:02 +0200

    Added register + writing to database

---
+++ src/main/java/com/onlineshopping/controllers/RegisterController.java
@@ -0,0 +1,39 @@
+package com.onlineshopping.controllers;
+
+import com.onlineshopping.model.User;
+import com.onlineshopping.repositories.UserRepository;
+import com.onlineshopping.services.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RegisterController {
+    private final UserService userService;
+    private final UserRepository userRepository;
+
+    public RegisterController(UserService userService, UserRepository userRepository) {
+        this.userService = userService;
+        this.userRepository = userRepository;
+    }
+
+    @RequestMapping("/register")
+    public String login() {
+        return "register";
+    }
+
+    @RequestMapping("/registerCheck")
+    public String check(@ModelAttribute("username") String username, @ModelAttribute("password") String password, final Model model){
+        User u = new User(username,password,"client");
+        userRepository.save(u);
+        return "succsess";
+
+    }
+
+    @RequestMapping("/register-error")
+    public String loginError(final Model model) {
+        model.addAttribute("registerError", true);
+        return "register";
+    }
+}
