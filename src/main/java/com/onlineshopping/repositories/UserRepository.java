afe4d3dfdcd0d6d7f376f124664c05b4721a830d
alexhprime <haragas6@gmail.com>
2021-12-20 18:54:11 +0200

    working login

--- src/main/java/com/onlineshopping/repositories/UserRepository.java
+++ src/main/java/com/onlineshopping/repositories/UserRepository.java
@@ -4,5 +4,8 @@ package com.onlineshopping.repositories;
 import com.onlineshopping.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User,Integer> {
+    public User findByUsername(String username);
 }
