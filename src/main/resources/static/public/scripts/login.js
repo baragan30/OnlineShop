1a1c71992ffbeb2aaefca5f68380ac89216d4310
baragan30 <baragan30@yahoo.com>
2021-12-21 01:31:34 +0200

    mere

--- src/main/resources/static/public/scripts/login.js
+++ src/main/resources/static/public/scripts/login.js
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
-const usernameInput = document.getElementsByName("uname")[0];
-const paswordInput = document.getElementsByName("psw")[0];
-const loginButton = document.getElementById("loginButton");
-const cancelButton = document.getElementById("cancelButton");
-const checkBox = document.getElementsByName('remember')[0];
-const errorLabel = document.getElementById('errorLabel');
-
-loginButton.addEventListener("click",onpressedLoginButton);
-=======
 const usernameInput = document.getElementById("username");
 const paswordInput = document.getElementById("password");
 const cancelButton = document.getElementById("cancelButton");
 const errorLabel = document.getElementById('errorLabel');
 
->>>>>>> 39ee00163e731d5c4ea3769f669edd61b192ebd8
 cancelButton.addEventListener("click",onpressedCancelButton);
 
 usernameInput.addEventListener("input" ,(e) => {
@@ -24,10 +13,6 @@ paswordInput.addEventListener("input" ,(e) => {
 });
 
 function onTypeInput(sourceInput,input){
-<<<<<<< HEAD
-=======
-
->>>>>>> 39ee00163e731d5c4ea3769f669edd61b192ebd8
     errorLabel.innerHTML = "";
     if (input === ""){
         errorLabel.innerHTML = sourceInput + " input is empty"
@@ -35,13 +20,6 @@ function onTypeInput(sourceInput,input){
 }
 
 
-<<<<<<< HEAD
-function onpressedLoginButton(e){
-    if(!(usernameInput.value === "") && !(paswordInput.value=== "") && checkBox.checked ){
-        console.log('Sent data');
-    }
-}
-=======
 // function onpressedLoginButton(e){
 //     if(!(usernameInput.value === "") && !(paswordInput.value=== "") ) {
 //         $.ajax({
@@ -61,7 +39,7 @@ function onpressedLoginButton(e){
 //         });
 //     }
 // }
->>>>>>> 39ee00163e731d5c4ea3769f669edd61b192ebd8
+
 function onpressedCancelButton(e){
     usernameInput.value = "";
     paswordInput.value = "";
