bc96e8cc4963d7d3b0712779b5d25ec73ad4c000
baragan30 <baragan30@yahoo.com>
2022-01-03 11:57:27 +0200

    mere

--- src/main/java/com/onlineshopping/model/Product.java
+++ src/main/java/com/onlineshopping/model/Product.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 @Table(name = "Products")
 public class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String description;
